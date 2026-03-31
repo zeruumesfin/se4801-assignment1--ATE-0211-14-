e02f18ce8de4cb112f0c9fda218e5391609c01eb
Zerubabel Mesfin <mesfinzeru96@gmail.com>
2026-03-31 12:13:42 +0300

    Initial Spring Boot project setup

---
+++ shopwave-starter/src/main/java/com/shopwave/shopwave_starter/ShopwaveStarterApplication.java
@@ -0,0 +1,13 @@
+package com.shopwave.shopwave_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopwaveStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopwaveStarterApplication.class, args);
+	}
+
+}
