bcba4e07975e8f20ae39f7917da10323be5a7786
Zerubabel Mesfin <mesfinzeru96@gmail.com>
2026-04-02 11:27:46 +0300

    Add ProductMapper

---
+++ src/main/java/com/shopwave/mapper/ProductMapper.java
@@ -0,0 +1,19 @@
+// Student Number: ATE/0211/14
+package com.shopwave.mapper;
+
+import com.shopwave.dto.ProductDTO;
+import com.shopwave.model.Product;
+
+public class ProductMapper {
+
+    public static ProductDTO toDTO(Product product) {
+        return ProductDTO.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .stock(product.getStock())
+                .categoryId(product.getCategory().getId())
+                .build();
+    }
+}
