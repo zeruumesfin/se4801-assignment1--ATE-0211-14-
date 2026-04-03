a1869207e679282f18d41bdeccd44d8130118ebc
Zerubabel Mesfin <mesfinzeru96@gmail.com>
2026-04-03 14:28:21 +0300

    Create test package for service layer

---
+++ src/test/java/com/shopwave/ShopwaveStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopwave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopwaveStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
